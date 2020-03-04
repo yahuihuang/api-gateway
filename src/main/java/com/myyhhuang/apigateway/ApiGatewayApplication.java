985bab48fde01707690502d785c210edabf1476f
J1040083 <huang.yahui.grace@gmail.com>
2020-03-04 14:42:44 +0800

    多Filter條件: 需傳入參數 accessToken 1.http://127.0.0.1:5555/api-feign-consumer/feign-consumer1?accessToken=111 2.http://127.0.0.1:5555/api-hello/hello?accessToken=111

--- src/main/java/com/myyhhuang/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/myyhhuang/apigateway/ApiGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -12,6 +13,11 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ApiGatewayApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 
 }
